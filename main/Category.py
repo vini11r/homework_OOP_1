9034d2407e344287ca27cbe439432953c877b1a7
Pavel Gornostaev <pavelgornostaeff@yandex.ru>
2024-03-06 20:02:19 +0300

    Режимы доступа

--- main/Category.py
+++ main/Category.py
@@ -1,15 +1,31 @@
+from homeworck_OOP_1.main.Product import Product
+
+
 class Category:
-    name = str
-    description = str
-    products = list
+    name: str
+    description: str
+    products: list
     number_of_categories = 0
     number_of_products = 0
 
     def __init__(self, name, description, product):
         self.name = name
         self.description = description
-        self.products = product
+        self.__products = product
 
         Category.number_of_categories += 1
-        Category.number_of_products += len(self.products)
+        Category.number_of_products += len(self.__products)
 
+    def add_product(self, obj_product):
+        """Добавляет объект товара в список товаров"""
+        self.__products.append(obj_product)
+        Category.number_of_products += 1
+
+    @property
+    def products(self):
+        """Возвращает список товаров с указанием цены и количества"""
+        products = ""
+        for i in self.__products:
+            product = f"{i.name}, {i.price} руб. Остаток: {i.quantity} шт.\n"
+            products += product
+        return products
--- main/Product.py
+++ main/Product.py
@@ -1,11 +1,27 @@
 class Product:
-    name = str
-    description = str
-    price = float
-    quantity = int
+    name: str
+    description: str
+    price: float
+    quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self._price = price
         self.quantity = quantity
+
+    @classmethod
+    def get_obj(cls, product_data):
+        """Возвращает список объектов класса Product"""
+        return cls(**product_data)
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print(f"Некорректная цена")
+        else:
+            self._price = new_price
